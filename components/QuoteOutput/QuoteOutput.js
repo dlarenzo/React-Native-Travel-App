ce1717ef71c801c37f086047197cbd7337ee4c1e
LaRenzo <dlarenzo@outlook.com>
2024-12-09 12:47:41 -0700

    created new branch 4.0-quotes

---
+++ components/QuoteOutput/QuoteOutput.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+function QuoteOutput() {
+  return (
+    <View>
+      {/* Quote Output */}
+      <Text>Quote Output</Text>
+    </View>
+  );
+}
+
+export default QuoteOutput;
